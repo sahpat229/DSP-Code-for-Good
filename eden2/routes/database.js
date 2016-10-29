0ee0bbe54c9f8ab33cf054a651c35d6b19a3884f
Force <force@ip-172-31-11-215.ec2.internal>
2016-10-29 08:32:17 +0000

    Setup urls for database queries

--- eden2/routes/database.js
+++ eden2/routes/database.js
@@ -5,12 +5,39 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "cooper"
+    password: "cooper",
+    database: "CooperCares"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
 });
 
 /* Handle requests to database */
 router.get('/', function(req, res, next) {
     res.send('hello world!');
+});
+
+router.get('/GET/Users', function(req, res, next) {
+    con.query('SELECT * FROM Users',function(err,rows){
+      if(err) throw err;
+
+      console.log('Data received from Db:\n');
+      res.send(rows);
+    });
+});
+
+router.get('/GET/Children', function(req, res, next) {
+    con.query('SELECT * FROM Children',function(err,rows){
+      if(err) throw err;
+
+      console.log('Data received from Db:\n');
+      res.send(rows);
+    });
 });
 
 module.exports = router;
