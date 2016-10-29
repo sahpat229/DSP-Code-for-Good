60e32227191a98c7561437c4abdb7c4718cbc46e
sigilance <sigilance@gmail.com>
2016-10-29 01:55:20 -0400

    Added kids page

--- eden2/routes/index.js
+++ eden2/routes/index.js
@@ -7,9 +7,11 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Eden II - Login' });
 });
 
-router.get('/main', function(req, res, next) {
-	res.render('main', { title: 'Main Page' });
-};
+router.get('/kids', function(req, res, next) {
+	// var listOfKids = getListOfKids ();
+
+	res.render('kids', { title: 'Kids Page' });
+});
 
 router.get('/interpret', function(req, res, next) {
 	res.render('interpret', { title: 'Interpret Speech', text: dbQueryForInterpretSpeech() });
