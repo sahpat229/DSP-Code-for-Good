be292cb8f5edbb850f5eff632d0ff30121897b35
Shalin Patel <shalinpatel75@gmail.com>
2016-10-29 01:21:58 -0400

    Moved database router to correct folder

--- eden2/routes/database.js
+++ eden2/routes/database.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cooper"
+});
 
 /* Handle requests to database */
 router.get('/', function(req, res, next) {
