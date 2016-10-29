d4e67181b4a56dd2d1ec8529b61cb276197f7b20
sigilance <sigilance@gmail.com>
2016-10-29 01:42:16 -0400

    Started on main page

--- eden2/routes/index.js
+++ eden2/routes/index.js
@@ -4,11 +4,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Eden II' });
+	res.render('index', { title: 'Eden II - Login' });
 });
 
+router.get('/main', function(req, res, next) {
+	res.render('main', { title: 'Main Page' });
+};
+
 router.get('/interpret', function(req, res, next) {
-	res.render('interpret', { title: 'Interpret', text: dbQueryForInterpretSpeech() });
+	res.render('interpret', { title: 'Interpret Speech', text: dbQueryForInterpretSpeech() });
 });
 
 module.exports = router;
