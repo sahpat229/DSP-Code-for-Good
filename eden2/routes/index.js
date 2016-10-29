9cb52fcb38d2a411830720707c96c48db7b18ce1
Force <force@ip-172-31-11-215.ec2.internal>
2016-10-29 06:51:12 +0000

    Setup kids view

--- eden2/routes/index.js
+++ eden2/routes/index.js
@@ -1,7 +1,23 @@
 'use strict';
-
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cooper",
+    database: "CooperCares"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
 
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Eden II - Login' });
@@ -9,8 +25,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/kids', function(req, res, next) {
 	// var listOfKids = getListOfKids ();
+    con.query('SELECT * FROM Children',function(err,rows){
+      if(err) throw err;
 
-	res.render('kids', { title: 'Kids Page' });
+      console.log('Data received from Db:\n');
+      console.log(rows);
+      res.render('kids', { title: 'Kids Page', children: rows });
+    });
+
 });
 
 router.get('/interpret', function(req, res, next) {
